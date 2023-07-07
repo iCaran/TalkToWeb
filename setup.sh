67b31e505e3931e68b74b6ccb5c04879a6a3cc74
karan pratap shaw <pratapmorgan@gmail.com>
2023-07-08 02:27:27 +1200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+echo "Python 3.8 is required for proper functioning of this program."
+echo "Please install Python 3.8. If already installed but not being detected,"
+echo "try adding Python 3.8 to the system path."
+echo ""
+
+# Installing Dependencies
+python3 -m pip install -r requirements.txt
